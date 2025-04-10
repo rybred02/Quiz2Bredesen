8c74e7cadeba75b8ea21ada9f0bf8cef610340eb
Ryan Bredesen <157738568+rybred02@users.noreply.github.com>
2025-04-10 14:40:28 -0500

    Add files via upload

---
+++ quiz 2.py
@@ -0,0 +1,35 @@
+import time
+from gpiozero import LED, Button, RGBLED
+
+
+buttonRed = Button(17)  
+buttonGreen = Button(27) 
+buttonBlue = Button(22)  
+
+red_led = LED(23)
+green_led = LED(24)
+blue_led = LED(25)
+
+
+while True:
+
+    if buttonRed.is_pressed:
+        red_led.on()  
+        print("Red button pressed! RGB LED is Red.")
+    else:
+        red_led.off()
+
+    if buttonGreen.is_pressed:
+        green_led.on() 
+        print("Green button pressed! RGB LED is Green.")
+    else:
+        green_led.off()
+
+    if buttonBlue.is_pressed:
+        blue_led.on()   
+        print("Blue button pressed! RGB LED is Blue.")
+    else:
+        blue_led.off()
+
+
+    time.sleep(0.1)
